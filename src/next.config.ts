743f0308b0d55df245abaf6cb4ded85abb78b05d
Srujal Patel <emperorsrujal@gmail.com>
2025-10-23 14:13:27 +0000

    rollback to version c66a3bf

--- src/next.config.ts
+++ src/next.config.ts
@@ -30,7 +30,8 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
       {
-        protocol: 'https',
+        protocol: 'https'
+        ,
         hostname: 'i.pravatar.cc',
         port: '',
         pathname: '/**',
